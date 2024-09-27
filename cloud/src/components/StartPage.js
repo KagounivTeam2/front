543f24108a5f0a07743b6125b24fc45a8848d4a3
durumi922 <durumi922@gmail.com>
2024-09-27 13:11:09 +0900

    디테일 수정 (헤더빼기, 버튼 라인빼기)

---
+++ cloud/src/components/StartPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './StartPage.css';
+
+function StartPage() {
+  return (
+    <div className="start-container">
+      <header className="header">
+      </header>
+      <img src="/img/start_img.png" alt="Logo" className="logo-image" />
+      <p>당신의 습관을 어쩌구</p>
+      <p>대충 감성있는 말 적기</p>
+      <div className="buttons">
+        <Link to="/my-habits">
+          <button className="habit-btn">나의 습관</button>
+        </Link>
+        <Link to="/add-habit">
+          <button className="habit-btn">습관 추가</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default StartPage;
