de44ee9c9332412925d552183974fc81f1086a5b
durumi922 <durumi922@gmail.com>
2024-09-27 10:12:28 +0900

    대략적인 페이지

--- cloud/src/App.js
+++ cloud/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import HabitView from './pages/HabitView';
+import StatisticsView from './pages/StatisticsView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/habit" component={HabitView} />
+        <Route path="/statistics" component={StatisticsView} />
+      </Switch>
+    </Router>
   );
 }
 
