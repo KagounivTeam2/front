c908fdd79ed66ca8e52893c71a1a899fc884cfea
durumi922 <durumi922@gmail.com>
2024-09-28 18:15:31 +0900

    텍스트, 택스트 색상, 이미지 변경

--- cloud/src/components/StartPage.js
+++ cloud/src/components/StartPage.js
@@ -7,9 +7,20 @@ function StartPage() {
     <div className="start-container">
       <header className="header">
       </header>
-      <img src="/img/start_img.png" alt="Logo" className="logo-image" />
-      <p>당신의 습관을 어쩌구</p>
-      <p>대충 감성있는 말 적기</p>
+      
+      {/* 텍스트 부분 */}
+      <div className="content-wrapper"> {/* 텍스트와 이미지를 감싸는 wrapper */}
+        <p className="intro-text">
+          좋은 습관을 설정하고, 매일 실천해보세요
+          <br />작은 변화가 큰 성장을 만들어줄 거예요
+        </p>
+      </div>
+
+        {/* 이미지 부분 */}
+      <img src="/img/start_2.png" alt="Logo" className="logo-image" />
+      
+
+      {/* 버튼 부분 */}
       <div className="buttons">
         <Link to="/my-habit">
           <button className="habit-btn">나의 습관</button>
