7ea5e3baa47ca72a007c5de774283c17139b2e1d
durumi922 <durumi922@gmail.com>
2024-09-28 22:35:34 +0900

    드디어 고침 감격이다

--- cloud/src/components/StartPage.js
+++ cloud/src/components/StartPage.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './StartPage.css';
+import styles from './StartPage.module.css'; // styles 객체로 import
 
 function StartPage() {
   return (
-    <div className="start-container">
-      <header className="header">
+    <div className={styles.startContainer}>  {/* className={styles.클래스명} */}
+      <header className={styles.header}>
       </header>
       
       {/* 텍스트 부분 */}
-      <div className="content-wrapper"> {/* 텍스트와 이미지를 감싸는 wrapper */}
-        <p className="intro-text">
+      <div className={styles.contentWrapper}> {/* styles 객체로 클래스 사용 */}
+        <p className={styles.introText}>
           좋은 습관을 설정하고, 매일 실천해보세요
           <br />작은 변화가 큰 성장을 만들어줄 거예요
         </p>
       </div>
 
-        {/* 이미지 부분 */}
-      <img src="/img/start_2.png" alt="Logo" className="logo-image" />
-      
+      {/* 이미지 부분 */}
+      <img src="/img/start_2.png" alt="Logo" className={styles.logoImage} />
 
       {/* 버튼 부분 */}
-      <div className="buttons">
+      <div className={styles.buttons}>
         <Link to="/my-habit">
-          <button className="habit-btn">나의 습관</button>
+          <button className={styles.habitBtn}>나의 습관</button>
         </Link>
         <Link to="/add-habit">
-          <button className="habit-btn">습관 추가</button>
+          <button className={styles.habitBtn}>습관 추가</button>
         </Link>
       </div>
     </div>
