70a49af4a2201e6f12ce05484d495f0bd21ec84e
yujeong <yujeong3389@gmail.com>
2024-09-29 02:11:38 +0900

    로그인 배경변경

--- cloud/src/components/Home.js
+++ cloud/src/components/Home.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css'; // 스타일을 위한 CSS
 
 function Home() {
+
+  const [background, setBackground] = useState('');
+
+  // 시간에 따른 배경색 설정 함수
+  const setTimeBasedBackground = () => {
+    const currentHour = new Date().getHours(); // 현재 시간 가져오기
+
+    if (currentHour >= 9 && currentHour < 15) {
+      // 09:00 ~ 15:00
+      setBackground('linear-gradient(to bottom, #FEA0B8, #FEE8D4)'); 
+    } else if (currentHour >= 15 && currentHour < 20) {
+      // 15:00 ~ 20:00
+      setBackground('linear-gradient(to bottom, #79CCFF, #D5FCFF)');
+    } else {
+      // 20:00 ~ 06:00
+      setBackground('linear-gradient(to bottom, #635FB8, #E2DAC7)');
+    }
+  };
+
+  useEffect(() => {
+    // 컴포넌트 마운트 시 배경색 설정
+    setTimeBasedBackground();
+
+    // 매 분마다 배경색을 업데이트하는 타이머 설정
+    const timer = setInterval(setTimeBasedBackground, 1000 * 60);
+
+    // 컴포넌트 언마운트 시 타이머 정리
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className="home-container">
+    <div className="home-container" style={{ background }}>
       <img src="/img/Logo_img.png" alt="Logo" className="logo-image" />
       <div className="buttons">
         <Link to="/login">
